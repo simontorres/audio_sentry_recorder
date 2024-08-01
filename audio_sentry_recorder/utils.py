6cac4823cdfab056eec53d17a8ea2a1114dcc8b0
Simon Torres <simon.torres@noirlab.edu>
2024-08-01 19:30:47 -0400

    Add logging

--- audio_sentry_recorder/utils.py
+++ audio_sentry_recorder/utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import logging
 
@@ -9,7 +10,8 @@ from pathlib import Path, PurePath
 def get_args(arguments=None):
     log = logging.getLogger()
     
-    parser = argparse.ArgumentParser(description="Records audio to a file only when the signal strength is above a given threshold.")
+    parser = argparse.ArgumentParser(
+        description="Records audio to a file only when the signal strength is above a given threshold.")
 
     parser.add_argument('--save-to-folder',
                         action='store',
@@ -39,3 +41,24 @@ def get_filename(start: datetime, end: datetime, save_to_folder: Path, format: s
 
     filename = folder_name.joinpath(f"{name_start}_{name_end}.{format}")
     return filename, folder_name
+
+
+def setup_logging(debug=False):
+    if '--debug' in sys.argv or debug:
+        log_format = '[%(asctime)s][%(levelname)8s]: %(message)s ' \
+                     '[%(module)s.%(funcName)s:%(lineno)d]'
+        logging_level = logging.DEBUG
+    else:
+        log_format = '[%(asctime)s][%(levelname).1s]: %(message)s'
+        logging_level = logging.INFO
+
+    date_format = '%H:%M:%S'
+
+    formatter = logging.Formatter(fmt=log_format,
+                                  datefmt=date_format)
+
+    logging.basicConfig(level=logging_level,
+                        format=log_format,
+                        datefmt=date_format)
+
+    log = logging.getLogger(__name__)
